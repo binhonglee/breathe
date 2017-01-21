46e5fd1116bb40fd29e9e4261cd1412f3d31043a
BinHong Lee <binhong@binhong.me>
2017-01-21 08:46:45 -0500

    Working out the basic foundation

--- src/index.js
+++ src/index.js
@@ -1,21 +1,115 @@
-"use strict";
-var Alexa = require("alexa-sdk");
-var handlers = {
-    "RequestIntent": function () {
-        var self = this;
-        var speechOutput = "rundown or specific area";
-        self.emit(":tellWithCard", speechOutput, "intro", speechOutput);
+'use strict';
+
+function buildResponse(sessionAttributes, speechletResponse) {
+    return {
+        version: '1.0',
+        sessionAttributes,
+        response: speechletResponse,
+    };
+}
+
+function getWelcomeResponse(callback) {
+  const sessionAttributes = {};
+  const cardTitle = 'Welcome';
+  const speechOutput = 'Welcome! ' + 'Please tell me one thing you feel good about yourself today by saying, I felt good running 2km today';
+
+  const repromptText = 'Please tell me one thing you feel good about yourself today by saying, I felt good running 2km today';
+  const shouldEndSession = false;
+
+  callback(sessionAttributes,
+    buildSpeechletResponse(cardTitle, speechOutput, repromptText, shouldEndSession));
+}
+
+function buildSpeechletResponse(title, output, repromptText, shouldEndSession) {
+  return {
+    outputSpeech: {
+      type: 'PlainText',
+      text: output,
     },
-    
-};
-var Handler = (function () {
-    function Handler(event, context, callback) {
-        var alexa = Alexa.Handler(event, context);
-        alexa.appId = "breathe";
-        alexa.registerHandlers(handlers);
-        alexa.execute();
+    card: {
+      type:'Simple',
+      title: `SessionSpeechlet - ${title}`,
+      content: `SessionSpeechlet - ${output}`,
+    },
+    reprompt: {
+      outputSpeech: {
+        type:'PlainText',
+        text: repromptText,
+      },
+    },
+    shouldEndSession,
+  };
+}
+
+function onSessionStarted(sessionStartedRequest, session) {
+  console.log(`onSessionStarted requestId=${sessionStartedRequest.requestId}, sessionId=${session.sessionId}`);
+}
+
+function onSessionEnded(sessionEndedRequest, session) {
+    console.log(`onSessionEnded requestId=${sessionEndedRequest.requestId}, sessionId=${session.sessionId}`);
+    // Add cleanup logic here
+}
+
+function onLaunch(LaunchRequest, session, callback) {
+  console.log(`onLaunch requestId=${LaunchRequest.requestId}, sessionId=${session.sessionId}`);
+
+  getWelcomeResponse(callback);
+}
+
+function onIntent(intentRequest, session, callback) {
+  console.log(`onIntent requestId=${intentRequest.requestId}, sessionId=${session.sessionId}`);
+
+  const intent = intentRequest.intent;
+  const intentName = intentRequest.intent.name;
+
+  if (intentName === 'RequestIntent') {
+
+  } else if (intentName === 'CreateEventIntent') {
+
+  } else if (intentName === 'CreateTherapistIntent') {
+
+  } else if (intentName === 'CreateFriendIntent') {
+
+  } else {
+    throw new Error('Invalid intent');
+  }
+}
+
+exports.handler = (event, context, callback) => {
+    try {
+        console.log(`event.session.application.applicationId=${event.session.application.applicationId}`);
+
+        /**
+         * Uncomment this if statement and populate with your skill's application ID to
+         * prevent someone else from configuring a skill that sends requests to this function.
+         */
+        /*
+        if (event.session.application.applicationId !== 'amzn1.echo-sdk-ams.app.[unique-value-here]') {
+             callback('Invalid Application ID');
+        }
+        */
+
+        if (event.session.new) {
+            onSessionStarted({ requestId: event.request.requestId }, event.session);
+        }
+
+        if (event.request.type === 'LaunchRequest') {
+            onLaunch(event.request,
+                event.session,
+                (sessionAttributes, speechletResponse) => {
+                    callback(null, buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === 'IntentRequest') {
+            onIntent(event.request,
+                event.session,
+                (sessionAttributes, speechletResponse) => {
+                    callback(null, buildResponse(sessionAttributes, speechletResponse));
+                });
+        } else if (event.request.type === 'SessionEndedRequest') {
+            onSessionEnded(event.request, event.session);
+            callback();
+        }
+    } catch (err) {
+        callback(err);
     }
-    return Handler;
-}());
-exports.Handler = Handler;
-//# sourceMappingURL=index.js.map
+};
