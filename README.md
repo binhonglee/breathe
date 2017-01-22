5e0dfc97e0f393f494bcf07af489f77e2ed5a5e3
BinHong Lee <binhong@binhong.me>
2017-01-22 00:17:41 -0500

    Documentations - Minor cleanup on unused Utterances and IntentScheme - Comments are added for the index.js - README.md is updated

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# spartaHacks
+# Breathe
+
+This is an Alexa skill that takes in your messages and store them into an array in the cache. It will then pull up random string from the array and remind users about it.
+
+The following is a peek into the Sample Utterances:
+
+```
+RequestIntent tell me something
+RequestIntent remind me
+CreateEventIntent create a new moment of {Event}
+CreateEventIntent create a new memory of {Event}
+CreateEventIntent record this moment of {Event}
+CreateEventIntent record this memory of {Event}
+StopIntent stop
+```
+
+| Intent | Purpose |
+|:-------|:------|
+| RequestIntent | For the users to randomly get one of the messages they had previously recorded. |
+| CreateEventIntent | For the users to record a new message (event) that provided them with positive reinforcement. |
+| StopIntent | To end / quit the Alexa skill. |
+
+
+###### This project is made at [SpartansHack 17](https://17.spartahack.com/).
--- src/index.js
+++ src/index.js
@@ -8,57 +8,73 @@ exports.handler = function(event, context, callback){
     alexa.execute();
 };
 
+// Handler for all input
 var handlers = {
+  // LaunchRequest handler
   'LaunchRequest': function() {
+    // Providing users with example of how this is to be used
     const say = 'Welcome! ' + 'Please tell me one thing you feel good about yourself today by saying, create a new moment of, and then stating your moment';
     const repromptText = 'Please tell me one thing you feel good about yourself today by saying, create a new moment of, and then stating your moment';
+    // Emit the message
     this.emit(':ask', say, repromptText);
   },
 
+  // CreateEventIntent handler
   'CreateEventIntent': function() {
+    // Get the user input
     var newEventItem = this.event.request.intent.slots.Event;
+    // Initialize the speechOutput
     let speechOutput = '';
-    let repromptText = '';
 
-    if (newEventItem) {
+    // If the item exist
+    if (newEventItem && newEventItem.value) {
       const newEvent = newEventItem.value;
 
+      // If the array does not exist already, create one
       if (!this.attributes['events']) {
         this.attributes['events'] = [];
       }
 
+      // Add the new message to the array
       this.attributes['events'].push(newEvent);
       console.log(newEvent);
 
-      speechOutput = 'I have completed recording ' + newEvent + ' as a new event.';
-      repromptText = 'You can ask me to tell you all the recorded events by saying, remind me';
+      // Inform the user that the message is successfully recorded
+      speechOutput = 'I have completed recording ' + newEvent + ' as a new event.' + 'You can ask me to tell you all the recorded events by saying, remind me';
     } else {
+      // Inform the user of an error
       speechOutput = "I am not sure what you are talking about. Please try again.";
-      repromptText = "I am not sure what you are talking about. You can tell me to record an event by saying, create a new moment of feeling good running 2km today";
     }
 
-    this.emit(':ask', speechOutput, repromptText);
-  },
-
-  'RequestIntent': function() {
-    let speechOutput = '';
-
-    if (this.attributes['events']) {
-      var toReturnPos = Math.floor(Math.random() * this.attributes['events'].length);
-      let toReturn = this.attributes['events'][toReturnPos];
-      speechOutput = 'One of your recorded moments is that you were ' + toReturn;
-    } else {
-      speechOutput = 'There is no moments recorded yet. You can say, create a new moment of feeling good running 2km today';
-    }
-
+    // Emit the message
     this.emit(':ask', speechOutput);
   },
 
+  // RequestIntent handler
+  'RequestIntent': function() {
+    // Initialize the speechOutput
+    let speechOutput = '';
+
+    // If the array exist
+    if (this.attributes['events']) {
+      // Get a random number range according to the array size
+      var toReturnPos = Math.floor(Math.random() * this.attributes['events'].length);
+      // Get the message in the randomed index
+      let toReturn = this.attributes['events'][toReturnPos];
+      // Tell the user of a random message previously recorded
+      speechOutput = 'One of your recorded moments is that you were ' + toReturn;
+    } else {
+      // Inform the user of missing message
+      speechOutput = 'There is no moments recorded yet. You can say, create a new moment of feeling good running 2km today';
+    }
+
+    // Emit the message
+    this.emit(':ask', speechOutput);
+  },
+
+  // StopIntent handler
   'StopIntent': function () {
-      var say = '';
-
-      say = 'Goodbye';
-
-      this.emit(':tell', say );
+    // Tell user "Goodbye"
+    this.emit(':tell', 'Goodbye');
   }
 }
